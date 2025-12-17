1674401ef3815344b8d3f4d2117b1aa276fa808e
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2025-12-16 22:53:37 -0500

    chore: Delete duplicate folder.

--- olfaction_local/olfaction_scripts_old/3_BrainpipeScripts/1_Intra_scripts/compute_canada/run_all_subj2.sh
+++
@@ -1,4 +0,0 @@
-for sub in FERJ VACJ SEMC LEFC PIRJ CHAF
-do
-	sbatch --account=def-kjerbi --time=08:00:00 --job-name=job_$sub --mem=31G --ntasks-per-node=24 --nodes=1 sub_submission.sh $sub
-done
--- olfaction_local/olfaction_scripts_old/3_BrainpipeScripts/1_Intra_scripts/compute_canada/sub_submission.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sujet=$1
-module load python/3.6.3
-
-$HOME/python_3_6/bin/python $HOME/Intra_EM/LDA_Classif_Power_Low_High_R.py $sujet
-
